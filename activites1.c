427a1416d9d4efe96174b5f010f73c6bbdc15ded
AhmedYassineMorghad <morghadahmedyassine0@gmail.com>
2025-01-21 15:07:35 +0000

    Ajout du projet Hello World en C

---
+++ activites1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void afficherDate(void) {
+    printf("21 Janvier 2024\n");
+}
+
+int main() {
+    char nom[]="Yassine";
+    afficherDate();
+    printf("Bonjour, %s !\n", nom);
+    return 0;
+}
